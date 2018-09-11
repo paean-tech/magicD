2e1558b667b67527cd3ba98b1e918d3bbc1ff608
pjason47 <jason.yuen@paean.net>
2018-09-11 21:52:12 +0800

    1.0.0

--- index.js
+++ index.js
@@ -1,36 +1,120 @@
 #!/usr/bin/env node
-
 const program = require('commander')
 const glob = require('glob')
 const path = require('path')
+const { startsWith, mapKeys, chain, pick, pickBy, merge, template, omit, keys, includes, get, uniq } = require('lodash')
+const { readFileSync, existsSync, writeFileSync } = require('fs')
+const mkdirp = require('mkdirp')
 const transformFileSync = require('@babel/core').transformFileSync
+let scannedKeyList = []
 
 function getConfig() {
-  let config = {}
-  try {
-    config = require(path.join(process.cwd(), 'i18n.config.json'));
-  } catch (e) {
-    console.error(e)
-  }
-  return config
-}
+  return getConfig._config = (getConfig._config || (() => {
+    let config = {}
+    const cwd = process.cwd()
+    try {
+      config = require(path.join(cwd, 'i18n.config.json'))
+    } catch (e) { console.error(e) }
+    return {
+      moduleSourceName: config.moduleSourceName || 'i18next',
+      format: {
+        funcList: get(config, 'format.funcList', ['t'])
+      },
+      messageDir: config.messageDir || path.join(cwd, './i18n/{{lng}}/{{ns}}.json'),
+      removeUnusedKey: config.removeUnusedKey || false,
+      exclude: config.exclude || [],
+      dirtyPrefix: config.dirtyPrefix || '~',
+      sort: config.sort || true,
+      lngs: config.lngs || [],
+      ns: config.ns || 'translation',
+      defaultVal: config.defaultVal || '',
+      keySeparator: config.keySeparator || '.',
+      interpolate: config.interpolate || /{{([\s\S]+?)}}/g,
+      dest: config.dest || '.i18n/{{lng}}/{{ns}}.json',
+    }
+  })())
+ }
 
-function run(path) {
+function run(loc) {
   const config = getConfig()
-  const exclude = config.exclude || []
-  glob(`${path}/**/*.{js, jsx}`, { ignore: exclude.map(pattern => `${path}/${pattern}`)}, (error, files) => {
+  glob(`${loc}/**/*.{js, jsx}`, { ignore: config.exclude.map(pattern => `${path}/${pattern}`)}, (error, files) => {
     files.forEach(filename => {
       if (filename.includes('node_modules')) return
       if (filename.indexOf('_') !== -1) return
       transformFileSync(filename, {
         presets: [require.resolve('babel-preset-umi')],
-        plugins: [
-          ['babel-plugin-magicd', Object.assign({}, config)]
-        ]
+        plugins: [scan]
       })
     })
-
+    config.lngs.forEach(lng => {
+      const loc = path.relative(process.cwd(), template(config.dest, { interpolate: config.interpolate })({ lng, ns: config.ns }))
+      saveJsonFile(loc, scannedKeyList)
+    })
   })
 }
+
+function referencesImport(loc, mod, importedNames) {
+  if (!(loc.isIdentifier() || loc.isJSXIdentifier())) {
+    return false;
+  }
+  return importedNames.some((name) => loc.referencesImport(mod, name));
+}
+
+function existJsonFile (loc) {
+  const exist = existsSync(loc)
+  let translations = {}
+  if (exist) {
+    try {
+      translations = JSON.parse(readFileSync(loc))
+    } catch (e) {}
+  }
+  return translations
+}
+
+function saveJsonFile(loc, newKeys) {
+  const config = getConfig()
+  const oldTranslations = existJsonFile(loc)
+  let newTranslations = mapKeys(oldTranslations, (v, k) => {
+    if (config.removeUnusedKey === false) {
+      const nk = k.replace(new RegExp('^' + config.dirtyPrefix), '')
+      if (!includes(newKeys, nk)) {
+        return config.dirtyPrefix + nk
+      }
+      return nk
+    }
+    return k
+  })
+  newKeys.forEach(k => newTranslations[k] == null && (newTranslations[k] = config.defaultVal))
+
+  if (config.removeUnusedKey === true) newTranslations = pick(newTranslations, scannedKeyList)
+  if (config.sort === true) {
+    newTranslations = chain(newTranslations).toPairs().sortBy(0).fromPairs().value()
+  }
+  const dirtyTranslations = pickBy(newTranslations, (v, k) => startsWith(k, config.dirtyPrefix))
+  newTranslations = merge({}, dirtyTranslations, omit(newTranslations, keys(dirtyTranslations)))
+  mkdirp.sync(path.dirname(loc))
+  writeFileSync(loc, JSON.stringify(newTranslations, null, 2))
+  return newTranslations
+}
+
+
+function scan({ types: t }) {
+  return {
+    visitor: {
+      CallExpression (path, state) {
+        const config = getConfig()
+        if (referencesImport(path.get('callee'), config.moduleSourceName, config.format.funcList)){
+          const args = path.get('arguments')
+          if (!t.isStringLiteral(args[0])) return
+          const key = get(args, '[0].node.value', null)
+          if (key != null) {
+            scannedKeyList = uniq(scannedKeyList.concat([key]))
+          }
+        }
+      }
+    }
+  }
+}
+
 program.parse(process.argv)
 run(program.args[0] || '.')
