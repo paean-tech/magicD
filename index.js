040d1d8b4e484053b69d12177da3b9e9a6aa1df9
pjason47 <jason.yuen@paean.net>
2018-10-23 14:57:57 +0800

    fix and inc version

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function getConfig() {
       messageDir: config.messageDir || path.join(cwd, './i18n/{{lng}}/{{ns}}.json'),
       removeUnusedKey: config.removeUnusedKey || false,
       exclude: config.exclude || [],
-      dirtyPrefix: isEmpty(config.dirtyPrefix) ? config.dirtyPrefix : '~',
+      dirtyPrefix: isEmpty(config.dirtyPrefix) ? '~' : config.dirtyPrefix,
       sort: config.sort || true,
       lngs: config.lngs || [],
       ns: config.ns || 'translation',
